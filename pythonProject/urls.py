d4ff740aa6f9c6888147685cbd5db347c0cdb4b3
lucifer-b16 <bharathshan@outlook.com>
2024-07-11 20:32:01 +0530

    Final Commit

--- pythonProject/urls.py
+++ pythonProject/urls.py
@@ -21,5 +21,6 @@ from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('predict/', views.predict)
+    path('predict/', views.predict),
+    path('predict/result', views.result)
 ]
--- pythonProject/views.py
+++ pythonProject/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 def home(request):
     return render(request, 'home.html')
@@ -7,3 +12,29 @@ def home(request):
 
 def predict(request):
     return render(request, 'predict.html')
+
+
+def result(request):
+    data = pd.read_csv(r"C:\Users\bhara\Desktop\pythonProject\pythonProject\static\pythonProject\AdditionalSheets\diabetes - diabetes.csv")
+    x = data.drop("Outcome", axis=1)
+    y = data['Outcome']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    model = LogisticRegression()
+    model.fit(x_train, y_train)
+
+    val1 = float(request.GET['n1'])
+    val2 = float(request.GET['n2'])
+    val3 = float(request.GET['n3'])
+    val4 = float(request.GET['n4'])
+    val5 = float(request.GET['n5'])
+    val6 = float(request.GET['n6'])
+    val7 = float(request.GET['n7'])
+    val8 = float(request.GET['n8'])
+    pred = model.predict([[val1, val2, val3, val4, val5,val6,val7,val8]])
+    result1 =""
+    if pred == [1]:
+        result1 = "Positive"
+    else:
+        result1 = "Negative"
+    return render(request, 'predict.html', {"result2":result1})
